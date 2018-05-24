78a80afaba16a3b70b105a6bdfa4d7726c3a95f0
Andrew Pokrepo <andrei.pokrepo97@gmail.com>
2018-05-25 01:14:28 +0300

    Add files via upload

---
+++ Task01.java
@@ -0,0 +1,13 @@
+public class Task01 {
+
+    public static void main (String[] args) {
+        DataTypesTester.testByte();
+        DataTypesTester.testShort();
+        DataTypesTester.testChar();
+        DataTypesTester.testLong();
+        DataTypesTester.testInt();
+        DataTypesTester.testDouble();
+        DataTypesTester.testFloat();
+        DataTypesTester.testBoolean();
+    }
+}
